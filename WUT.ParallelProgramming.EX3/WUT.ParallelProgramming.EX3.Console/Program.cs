ef985500c6488239b0fe61910ec7563ed7a09e99
Bi3NioL <bi3niol@gmail.com>
2018-04-20 08:39:58 +0200

    EX3 init

---
+++ WUT.ParallelProgramming.EX3/WUT.ParallelProgramming.EX3.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUT.ParallelProgramming.EX3.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
