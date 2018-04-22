49b781f2be9bf96ae1e796eaf9f7231094794efa
Mateusz Bieńkowski <bi3niol@gmail.com>
2018-04-22 22:38:29 +0200

    server base

---
+++ WUT.ParallelProgramming.EX3/WUT.ParallelProgramming.ServerMQTT/Server.cs
@@ -0,0 +1,33 @@
+﻿using MQTTnet;
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUT.ParallelProgramming.ServerMQTT
+{
+    public class Server
+    {
+        private IMqttServer mqttServer;
+        private MqttServerOptionsBuilder optionsBuilder;
+        public Server()
+        {
+            mqttServer = new MqttFactory().CreateMqttServer();
+            optionsBuilder = new MqttServerOptionsBuilder();
+            optionsBuilder = optionsBuilder
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointPort(1884);
+        }
+        public async void Start()
+        {
+            await mqttServer.StartAsync(optionsBuilder.Build());
+        }
+
+        public async void Stop()
+        {
+            await mqttServer.StopAsync();
+        }
+    }
+}
